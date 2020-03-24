fd0760715e2c9fd0995ebeffe0f95d3fcc0c11b6
meyg8600 <meygen_cruz@dlsu.edu.ph>
2020-03-24 17:01:34 +0800

    Finalized automated snapping code for small, medium, and large boxes

--- snap_automated_testing.py
+++ snap_automated_testing.py
@@ -172,8 +172,8 @@ class Snap:
             fgbg = cv2.createBackgroundSubtractorKNN()
             fgbg.setShadowValue(0)
             
-            if frame_no > 500:
-                vid.set(cv2.CAP_PROP_POS_FRAMES, frame_no - 500)
+            if frame_no > 50:
+                vid.set(cv2.CAP_PROP_POS_FRAMES, frame_no - 50)
             else:
                 vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
             
@@ -231,7 +231,7 @@ class Snap:
         if event == cv2.EVENT_LBUTTONDOWN:
             print("x-coor: ", x, ", y-coor:", y)
 
-    def run(self,vid_name,FRAME_NO,bboxx1,bboxy1,bboxx2,bboxy2):
+    def run(self,vid_name,FRAME_NO,bboxx1,bboxy1,bboxx2,bboxy2,folder_name):
         # the video could only use up to 400 frames for high accuracy
         vid = cv2.VideoCapture(vid_name)
         
@@ -242,7 +242,6 @@ class Snap:
         (H,W) = img.shape[:2]
         
         imgvis = imutils.resize(img, width=1028)
-        folder_name = vid_name[7:-4]
         ratioW = W/1028
 
         x1, y1, x2, y2 = snap.snap_algorithm(snap.SNAP_BACKGROUND_SUBTRACTION, vid, FRAME_NO, bboxx1*ratioW, bboxy1*ratioW, bboxx2*ratioW, bboxy2*ratioW, folder_name)
@@ -257,40 +256,51 @@ class Snap:
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
-    ap.add_argument("-r", "--run_from_file", required=False, default="areas_used.txt", help="process coordinates from file")
+    ap.add_argument("-r", "--run_from_file", required=False, default=None, help="process coordinates from file")
     args = vars(ap.parse_args())
-
     snap = Snap()
     i = 1
-    with open (args["run_from_file"], 'rt') as myfile:
-        for line in myfile:
-            if i%8 == 1:
-                video = line.rstrip('\n').replace('vid name: ','')
-                folder_name = video[7:-4]
-                print('folder name: ' + folder_name)
-                if path.exists(folder_name) == False:
-                    os.mkdir(folder_name)
-                    print("directory made")
-            if i%8 == 2:
-                frame_no = int(line.rstrip('\n').replace('frame no: ',''))
-            if i%8 == 3:
-                x1 = int(line.rstrip('\n').replace('x1: ',''))
-            if i%8 == 4:
-                y1 = int(line.rstrip('\n').replace('y1: ',''))
-            if i%8 == 5:
-                x2 = int(line.rstrip('\n').replace('x2: ',''))
-            if i%8 == 6:
-                y2 = int(line.rstrip('\n').replace('y2: ',''))
-            if i%8 == 7:
-                print(video)
-                print(frame_no)
-                print(int(x1))
-                print(int(y1))
-                print(int(x2))
-                print(int(y2))
-            if i%8 == 0:
-                snap.run(video,frame_no,x1,y1,x2,y2)
-                print("\n\n")
-            i = i+1
+
+    if args["run_from_file"] == None:
+        args["run_from_file"] = ["small_boxes.txt", "medium_boxes.txt", "large_boxes.txt"]
+    else:
+        args["run_from_file"] = [args["run_from_file"]]
+    
+    for filename in args["run_from_file"]:
+        if path.exists(filename[0:-4]) == False:
+            os.mkdir(filename[0:-4])
+            print(str(filename[0:-4]) + " directory made")
+        with open (filename, 'rt') as myfile:
+            for line in myfile:
+                if i%8 == 1:
+                    video = line.rstrip('\n').replace('vid name: ','')
+                    folder_name = str(filename[0:-4]) + "/" + str(video[7:-4])
+                    print(folder_name)
+                    print('folder name: ' + folder_name)
+                    if path.exists(folder_name) == False:
+                        os.mkdir(folder_name)
+                        print(str(folder_name) + " directory made")
+                if i%8 == 2:
+                    frame_no = int(line.rstrip('\n').replace('frame no: ',''))
+                if i%8 == 3:
+                    x1 = int(line.rstrip('\n').replace('x1: ',''))
+                if i%8 == 4:
+                    y1 = int(line.rstrip('\n').replace('y1: ',''))
+                if i%8 == 5:
+                    x2 = int(line.rstrip('\n').replace('x2: ',''))
+                if i%8 == 6:
+                    y2 = int(line.rstrip('\n').replace('y2: ',''))
+                if i%8 == 7:
+                    print(video)
+                    print(frame_no)
+                    print(int(x1))
+                    print(int(y1))
+                    print(int(x2))
+                    print(int(y2))
+                if i%8 == 0:
+
+                    snap.run(video,frame_no,x1,y1,x2,y2,folder_name)
+                    print("\n\n")
+                i = i+1
                 
  
