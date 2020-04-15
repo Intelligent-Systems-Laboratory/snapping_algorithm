187ad9536c23795f9f92c8ef62e169f77cd251ee
RDeticio <ramielgdeticio@gmail.com>
2020-04-15 19:27:41 +0800

    Added text file reader and accuracy measure

--- snap.py
+++ snap.py
@@ -428,7 +428,7 @@ class Snap:
                     px2 = int(x1 + nX + w)
                     py2 = int(y1 + nY + h)
                     cv2.rectangle(frame, (px1, py1), (px2, py2), (0, 0xFF, 0), 4)
-                    return thresh, fgmask_crop, frame_crop, frame
+                    return thresh, fgmask_crop, frame_crop, frame, px1, px2, py1, py2
 
 
         # snapping with CNT background subtraction implementation
--- snap_automated_testing.py
+++ snap_automated_testing.py
@@ -9,6 +9,7 @@ from os import path
 
 from snap import Snap
 
+from array import *
 
 def time():
     dateTimeObj = datetime.now()
@@ -19,6 +20,50 @@ def time_print(dateTimeObj):
     print(dateTimeObj.year, '/', dateTimeObj.month, '/', dateTimeObj.day)
     print(dateTimeObj.hour, ':', dateTimeObj.minute, ':', dateTimeObj.second, '.', dateTimeObj.microsecond)
 
+# Object definition of a test case
+class Case(object):
+    def __init__(self, videoFile, frame, x1, x2, y1, y2):
+        self.videoFile = videoFile
+        self.frame = frame
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+    def printCase(self):
+        print("video: " + str(self.videoFile))
+        print("frame: " + str(self.frame))
+        print("x1: " + str(self.x1))
+        print("y1: " + str(self.y1))
+        print("x2: " + str(self.x2))
+        print("y2: " + str(self.y2))
+
+
+# Formats text file to an array for easier access
+def textFileToCaseList(filename):
+    caseList = []
+    i = 1
+    with open (filename, 'rt') as myfile:
+        for line in myfile:
+            if i%8 == 1:
+                video = line.rstrip('\n').replace('vid name: ','')
+            if i%8 == 2:
+                frame_no = int(line.rstrip('\n').replace('frame no: ',''))
+            if i%8 == 3:
+                x1 = int(line.rstrip('\n').replace('x1: ',''))
+            if i%8 == 4:
+                y1 = int(line.rstrip('\n').replace('y1: ',''))
+            if i%8 == 5:
+                x2 = int(line.rstrip('\n').replace('x2: ',''))
+            if i%8 == 6:
+                y2 = int(line.rstrip('\n').replace('y2: ',''))
+                caseList.append(Case(video, frame_no, x1, x2, y1 ,y2))
+                        
+            i = i+1
+
+    return caseList
+
+# Measures accuracy by finding the ratio between the intersection and union of generated bounding box and the ground truth bounding box
 def measureAccuracy (truth_xmin, truth_xmax, truth_ymin, truth_ymax, box_xmin, box_xmax, box_ymin, box_ymax):
     truthArea = (truth_xmax - truth_xmin) * (truth_ymax - truth_ymin)
     boxArea = (box_xmax - box_xmin) * (box_ymax - box_ymin)
@@ -28,12 +73,12 @@ def measureAccuracy (truth_xmin, truth_xmax, truth_ymin, truth_ymax, box_xmin, b
     topIntersect = min(truth_ymax, box_ymax)
 
     if (rightIntersect < leftIntersect) or (topIntersect < bottomIntersect):
-        overlapArea = 0
+        accuracy = 0
     else:
-        overlapArea = (rightIntesect - leftIntersect) * (topIntersect - bottomIntersect)
+        overlapArea = (rightIntersect - leftIntersect) * (topIntersect - bottomIntersect)
         totalArea = truthArea + boxArea - overlapArea
+        accuracy = overlapArea / totalArea
 
-    accuracy = overlapArea / totalArea
     return accuracy
 
 def run(vid_name,FRAME_NO,bboxx1,bboxy1,bboxx2,bboxy2,folder_name,snap_type):
@@ -47,8 +92,10 @@ def run(vid_name,FRAME_NO,bboxx1,bboxy1,bboxx2,bboxy2,folder_name,snap_type):
     (H, W) = img.shape[:2]
     imgvis = imutils.resize(img, width=1028)
     ratioW = W/1028
+
+    # Return bbox coordinates along with generated images
     if snap_type == snap.SNAP_BACKGROUND_SUBTRACTION_KNN or snap_type == snap.SNAP_BACKGROUND_SUBTRACTION_MOG2 or snap_type == snap.SNAP_BACKGROUND_SUBTRACTION_CNT or snap.SNAP_BACKGROUND_SUBTRACTION_CNT_NO_SHADOW:
-        thresh, fgmask_crop, frame_crop, imgvis = snap.snap_algorithm(snap_type, vid, FRAME_NO, bboxx1*ratioW, bboxy1*ratioW, bboxx2*ratioW, bboxy2*ratioW)
+        thresh, fgmask_crop, frame_crop, imgvis, box_xmin, box_xmax, box_ymin, box_ymax = snap.snap_algorithm(snap_type, vid, FRAME_NO, bboxx1*ratioW, bboxy1*ratioW, bboxx2*ratioW, bboxy2*ratioW)
     elif snap_type == snap.SNAP_GRABCUT:
         thresh, grabcut_crop, display = snap.snap_algorithm(snap_type, imgvis, bboxx1*ratioW, bboxy1*ratioW, bboxx2*ratioW, bboxy2*ratioW)
 
@@ -64,6 +111,14 @@ def run(vid_name,FRAME_NO,bboxx1,bboxy1,bboxx2,bboxy2,folder_name,snap_type):
         cv2.imwrite(folder_name+'/frame_'+str(frame_no)+'_crop_grabcut.jpg', grabcut_crop)
         cv2.imwrite(folder_name+'/frame_'+str(FRAME_NO)+'_grabcut.jpg',display)
 
+    # Downsize generated bounding box coordinates
+    box_xmin /= ratioW
+    box_xmax /= ratioW
+    box_ymin /= ratioW
+    box_ymax /= ratioW    
+
+    return box_xmin, box_xmax, box_ymin, box_ymax
+
 
 start_time = time()
 ap = argparse.ArgumentParser()
@@ -71,15 +126,72 @@ ap.add_argument("-r", "--run_from_file", required=False, default=None, help="pro
 args = vars(ap.parse_args())
 
 snap = Snap()
-snap_type = snap.SNAP_BACKGROUND_SUBTRACTION_CNT_NO_SHADOW
+snap_type = snap.SNAP_BACKGROUND_SUBTRACTION_MOG2
 i = 1
+
+# Sets list of ground truth bounding boxes
+groundTruthFileName = "ground_truth.txt"
+groundTruthList = textFileToCaseList(groundTruthFileName)
 
 if args["run_from_file"] == None:
     args["run_from_file"] = ["small_boxes.txt"]
 else:
     args["run_from_file"] = [args["run_from_file"]]
-    
+
 for filename in args["run_from_file"]:
+
+    if path.exists(filename[0:-4]) == False:
+        os.mkdir(filename[0:-4])
+        print(str(filename[0:-4]) + " directory made")
+
+    smallBoxesList = textFileToCaseList(filename)
+    for case in smallBoxesList:
+        # Initialize properties of test case
+        video = case.videoFile
+        frame_no = case.frame
+        x1 = case.x1
+        y1 = case.y1
+        x2 = case.x2
+        y2 = case.y2
+        folder_name = str(filename[0:-4]) + "/" + str(case.videoFile[7:-4])
+
+        case.printCase()
+        print(folder_name)
+
+        if path.exists(folder_name) == False:
+            os.mkdir(folder_name)
+            print(str(folder_name) + " directory made")
+
+        # Retrieve corresponding ground truth case
+        index = smallBoxesList.index(case)
+        truth = groundTruthList[index]
+        truth_x1 = truth.x1
+        truth_x2 = truth.x2
+        truth_y1 = truth.y1
+        truth_y2 = truth.y2
+
+        # Run snapping algorithm
+        box_xmin, box_xmax, box_ymin, box_ymax = run(video,frame_no,x1,y1,x2,y2,folder_name,snap_type)
+
+        # Measure accuracy of adjusted bounding box
+        accuracy = measureAccuracy(truth_x1, truth_x2, truth_y1, truth_y2, box_xmin, box_xmax, box_ymin, box_ymax)
+        print(str(accuracy*100) + '%')
+
+        print("\n\n")
+
+'''
+for case in groundTruthList:
+    print("video: " + str(case.videoFile))
+    print("frame: " + str(case.frame))
+    print("x1: " + str(case.x1))
+    print("y1: " + str(case.y1))
+    print("x2: " + str(case.x2))
+    print("y2: " + str(case.y2))
+
+    print("\n\n")
+'''
+
+'''
     if path.exists(filename[0:-4]) == False:
         os.mkdir(filename[0:-4])
         print(str(filename[0:-4]) + " directory made")
@@ -111,10 +223,19 @@ for filename in args["run_from_file"]:
                 print(int(x2))
                 print(int(y2))
             if i%8 == 0:
-                run(video,frame_no,x1,y1,x2,y2,folder_name,snap_type)
+                box_xmin, box_xmax, box_ymin, box_ymax = run(video,frame_no,x1,y1,x2,y2,folder_name,snap_type)
+                
+                # Printing accuracy
+                accuracy = measureAccuracy (x1, x2, y1, y2, box_xmin, box_xmax, box_ymin, box_ymax)
+                print(box_xmin)
+                print(box_xmax)
+                print(box_ymin)
+                print(box_ymax)
+                print(accuracy)
+                
                 print("\n\n")
             i = i+1
-
+'''
 end_time = time()
 
 time_print(start_time)
